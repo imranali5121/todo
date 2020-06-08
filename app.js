233ca1ce0005df86e9814fb87cdb9c1ea5db9296
Imran <imranali5121@github.com>
2020-06-08 22:05:05 +0500

    edit button added

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 
 var container = document.getElementById("todo-container");
-var todos = [
-    // { title: "todo 1" },
-    // { title: "todo 2" },
-    // { title: "todo 3" },
-]
-
+var todos = [];
+var editIndex = null
+var input = document.getElementById("task");
 
 function render() {
     container.innerHTML = "";
     for (var i = 0; i < todos.length; i++) {
         var element = todos[i];
         container.innerHTML += `<p>${element.title} 
-        <button onclick="onDelete(${i})">Delete</button></p>`
+        <button onclick="onDelete(${i})">Delete</button>
+        <button onclick="onEdit(${i})">edit</button>
+        </p>`
     }
 }
 
 function addTodo() {
-    todos.push({ title: "New Todo" });
+    if(editIndex !== null){
+        todos[editIndex] = { title: input.value }
+    }else{
+        todos.push({ title: input.value });
+    }
+    input.value = ""
+    editIndex = null;
     render();
 }
 
 function onDelete(i) {
     todos.splice(i, 1);
     render();
+};
+function onEdit(i) {
+    var element = todos[i];
+    input.value = element.title;
+    editIndex = i
 }
 
 render();
