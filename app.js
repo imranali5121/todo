c1b6f504a95159c41aa17d44f0d492c79864793c
Imran <imranali5121@github.com>
2020-06-09 00:38:48 +0500

    check box added

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ function render() {
     container.innerHTML = "";
     for (var i = 0; i < todos.length; i++) {
         var element = todos[i];
-        container.innerHTML += `<p>${element.title} 
+        container.innerHTML += `<p class="${element.check ? 'done' : 'notdone'}">
+        <input type="checkbox" onclick="onCheck(${i})" ${element.check && "checked"} >
+        ${element.title} 
        <i class="fa fa-trash" onclick="onDelete(${i})"></i>
        <i class="fa fa-edit" onclick="onEdit(${i})"></i>
         </p>`
@@ -16,10 +18,10 @@ function render() {
 }
 
 function addTodo() {
-    if(editIndex !== null){
-        todos[editIndex] = { title: input.value }
-    }else{
-        todos.push({ title: input.value });
+    if (editIndex !== null) {
+        todos[editIndex] = { title: input.value, check: false }
+    } else {
+        todos.push({ title: input.value, check: false });
     }
     input.value = ""
     editIndex = null;
@@ -35,6 +37,11 @@ function onEdit(i) {
     input.value = element.title;
     input.focus();
     editIndex = i
+}
+function onCheck(i) {
+    var element = todos[i];
+    element.check = !element.check;
+    render();
 }
 
 render();
